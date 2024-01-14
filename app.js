500fa2cf0b82802bd24628acb9ddf0b797d0b40d
moheet333 <moheetshendarkar@gmail.com>
2024-01-15 00:21:17 +0530

    UPDATE: working on login

--- app.js
+++ app.js
@@ -6,16 +6,23 @@ const path = require("path");
 const cors = require("cors");
 
 const app = express();
+app.use(express.static(path.resolve(__dirname, "./client/build/")));
 app.use(cors());
 app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 // routers
 const authRouter = require("./routes/auth");
+const notFoundMiddleware = require("./middleware/not-found");
 
 app.use("/api/v1/auth", authRouter);
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+app.use(notFoundMiddleware);
 
 const start = async () => {
   try {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,37 @@
 const { BadRequestError } = require("../errors");
+const supabase = require("../db/connect");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
 
 const registerUser = async (req, res) => {
-  const { username, email, password } = req.body;
+  let { username, email, password } = req.body;
+
   if (!username || !email || !password) {
     throw new BadRequestError("Please pass all fields");
   }
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  password = await bcrypt.hash(password, salt);
+  // insert data
+  const { data, error } = await supabase
+    .from("user")
+    .insert({ username: username, email: email, password: password })
+    .select();
+  // generate token
+  const token = jwt.sign(
+    { userId: data[0].id, username: data[0].username },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRESIN,
+    }
+  );
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      username: data[0].username,
+      token,
+    },
+  });
 };
 
 module.exports = {
